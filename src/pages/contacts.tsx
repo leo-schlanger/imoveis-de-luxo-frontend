3a0cf43ef3b0ada54bf72dc2034bfb4088b14a5c
Schlanger <leoschlanger@gmail.com>
2020-11-08 11:54:37 -0300

    contacts

---
+++ src/pages/contacts.tsx
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import React, { useRef } from 'react';
+import { Form } from '@unform/web';
+import { FiUser, FiMail, FiPhone } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
+import { Container } from '../../styles/pages/Contacts';
+import Input from '../components/Input';
+import Button from '../components/Button';
+
+const Terms: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  return (
+    <Container>
+      <h1>Contatos</h1>
+      <p>
+        Envie seus pedidos, dúvidas ou sugestões, através do formulário abaixo
+        ou por telefone.
+      </p>
+
+      <Form ref={formRef} onSubmit={() => {}}>
+        <Input name="name" icon={FiUser} placeholder="Nome" />
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
+        <Input name="phone" icon={FiPhone} placeholder="Telefone" />
+        <textarea name="message" placeholder="Mensagem" />
+        <Button>Enviar</Button>
+      </Form>
+    </Container>
+  );
+};
+
+export default Terms;
---
+++ styles/pages/Contacts.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 64px;
+  font-size: 16px;
+
+  input {
+    margin-top: 10px;
+  }
+  form{
+  margin-top:10px;
+  }
+
+  textarea{
+    padding: 10px;
+    line-height: 1.5;
+    border-radius: 5px;
+    border: 1px solid #ccc;
+    box-shadow: 1px 1px 1px #999;
+  }
+`;
