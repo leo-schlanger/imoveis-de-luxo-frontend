d4f4208e22579e3a23376ee518a27ea90df0526c
Schlanger <leoschlanger@gmail.com>
2020-11-08 12:39:04 -0300

    update package

--- src/pages/advertisements/[id].tsx
+++ src/pages/advertisements/[id].tsx
@@ -1,17 +1,19 @@
-import { Button } from '@material-ui/core'
-import {useRouter} from 'next/router';
-import Head from 'next/head'
+import { Button } from '@material-ui/core';
+import { useRouter } from 'next/router';
+import Head from 'next/head';
 
-export default function Home() {
-  const {query} = useRouter();
+export default function Home(): JSX.Element {
+  const { query } = useRouter();
   return (
     <>
-    <Head>
-      <title>{query.id}</title>
-    </Head>
-    <div>
-      <Button variant="contained" color="primary">Detalhes do pedido {query.id}!!</Button>
-    </div>
+      <Head>
+        <title>{query.id}</title>
+      </Head>
+      <div>
+        <Button variant="contained" color="primary">
+          Detalhes do pedido {query.id}!!
+        </Button>
+      </div>
     </>
-  )
+  );
 }
