9d728b8578afca1e5a5236b11ce18c97131da266
natan <natan_rv2@hotmail.com>
2020-11-08 17:13:50 -0300

    advertisement-details

---
+++ src/pages/advertisement-details/[id].tsx
@@ -0,0 +1,140 @@
+import { Button } from '@material-ui/core';
+import { useRouter } from 'next/router';
+import Head from 'next/head';
+import { useState, useEffect } from 'react';
+
+import { FiHome } from 'react-icons/fi';
+
+import { useQuery } from '@apollo/react-hooks';
+import { FaBed, FaParking, FaShower } from 'react-icons/fa';
+import { IoIosCube } from 'react-icons/io';
+import {
+  Container,
+  Adescription,
+  Adinfo,
+  Adseller,
+  GridList,
+  GridListTile,
+  GridListTileBar,
+  GridHolder,
+} from '../../../styles/pages/AdvertisementsDetails';
+
+
+
+import { Advertisement } from '../../libs/entities/advertisements';
+import { FIND_ADVERTISEMENT_BY_ID } from '../../libs/gql/advertisements';
+import Image from 'next/image';
+
+interface IQueryData {
+  getAdvertisementById: Advertisement;
+}
+
+const AdvertisementDetails: React.FC = () => {
+  const [advertisement, setAdvertisement] = useState<
+    Advertisement | undefined
+  >();
+  const { query } =  useRouter();
+  
+  const { data , error} = useQuery<IQueryData>(FIND_ADVERTISEMENT_BY_ID, {
+    variables: { id: parseInt(String(query.id) ,10) },
+  });
+
+  useEffect(() => {
+    setAdvertisement(data?.getAdvertisementById);
+    console.log(error);
+  }, [data, error]);
+
+  return (
+    <Container>
+       <GridHolder  >
+      <GridList cols={2.5}>
+        
+          <GridListTile >
+          <img
+                    src="/assets/casa.svg"
+                    alt="house"
+                    
+                  />
+          </GridListTile>
+          <GridListTile >
+          <img
+                    src="/assets/casa.svg"
+                    alt="house"
+                    
+                  />
+          </GridListTile>
+          <GridListTile >
+          <img
+                    src="/assets/casa.svg"
+                    alt="house"
+                    
+                  />
+          </GridListTile>
+          <GridListTile >
+          <img
+                    src="/assets/casa.svg"
+                    alt="house"
+                   
+                  />
+                  
+          </GridListTile>
+        </GridList>
+    </GridHolder>
+        {/* <Carousel
+          showThumbs
+          dynamicHeight={false}
+          width="100%"
+          thumbWidth={200}
+        >
+          {advertisement &&
+            advertisement.gallery.map((item) => (
+              <img alt="image3" src={item.url} />
+            ))}
+        </Carousel> */}
+       <Adinfo>
+        {/* {advertisements.map(advertisement =>
+            <li>
+            <h6>{advertisement.title}</h6>
+            <img src={advertisement.gallery[0].filename} alt="house" />
+            <p>{advertisement.property.value}</p>
+
+          </li>
+            )} */}
+        <FaBed size="2rem" color="#c1b994" />
+        <p>infos</p>
+        <FaParking size="2rem" color="#c1b994" />
+        <p>infos</p>
+        <FaShower size="2rem" color="#c1b994" />
+        <p>infos</p>
+        <IoIosCube size="2rem" color="#c1b994" />
+        <p>infos</p>
+      </Adinfo>
+      <Adseller>
+        <FiHome size="2rem" color="#c1b994" />
+        <p>infos</p>
+      </Adseller>
+      <Adescription> 
+        <hr/>
+          <p>
+          Endereço:
+          <br />
+          {advertisement &&
+            `${advertisement.property.address.country} ${advertisement.property.address.state} ${advertisement.property.address.neighborhood} ${advertisement.property.address.number}`}
+        </p>
+        <hr/>
+        <p>
+          Descrição:
+          <br />
+          {advertisement && advertisement.description}
+        <hr/>
+        </p>
+
+       
+      </Adescription>
+     
+    
+    </Container>
+  );
+};
+
+export default AdvertisementDetails;
---
+++ styles/pages/AdvertisementsDetails.ts
@@ -0,0 +1,122 @@
+import styled from 'styled-components';
+import GridListMaterialUI from '@material-ui/core/GridList';
+import GridListTileMaterialUI from '@material-ui/core/GridListTile';
+import GridListTileBarMaterialUI from '@material-ui/core/GridListTileBar';
+
+
+interface AdvertisementProps {
+  background_url: string;
+}
+
+export const Advertisement = styled.div<AdvertisementProps>`
+  display: flex;
+  flex: 1;
+  background: url(${(props) => props.background_url});
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  padding: 20px 0px;
+  margin-bottom: 20%;
+  height: 1200px;
+  img {
+    width: 100%;
+    height: 300px;
+  }
+`;
+
+export const Adescription = styled.div`
+  width: 80%;
+  p {
+    margin-top: 2%;
+    width: 100%;
+    height: 60px;
+    font-size:16px;
+    color: #c1b994;
+  }
+`;
+
+
+export const Carrosel = styled.div`
+  width: 80%;
+  place-items: center;
+ 
+`;
+
+export const Adinfo = styled.div`
+  display: grid;
+  place-items: center;
+  margin-top: 5%;
+  margin-bottom: 5%;
+  width: 80%;
+  grid-template-columns: repeat(auto-fit, minmax(15px, 1fr));
+  p {
+    height: 40px;
+  }
+`;
+
+export const Adseller = styled.div`
+  display: grid;
+  place-items: center;
+  margin-top: 1%;
+  margin-bottom: 5%;
+  width: 80%;
+`;
+
+export const Adlist = styled.div`
+  display: grid;
+  width: 80%;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 24px;
+  list-style: none;
+  ul li {
+    background: #fff;
+    padding: 24px;
+    border-radius: 8px;
+    position: relative;
+  }
+  h6 {
+    color: green;
+    margin-bottom: 0px;
+  }
+
+  img {
+    width: 200px;
+    height: 200px;
+  }
+
+  li {
+    padding: 24px;
+    border-radius: 4px;
+    position: relative;
+    background-color: #c1b994;
+  }
+`;
+
+export const GridHolder = styled.div`
+display: flex;
+flexWrap: wrap;
+flex-direction:row;
+width:80%;
+justifyContent: space-around;
+overflow: hidden;
+backgroundColor: theme.palette.background.paper;
+`;
+
+export const GridList = styled(GridListMaterialUI)`
+flexWrap: nowrap;
+transform: translateZ(0);
+`;
+
+export const GridListTile = styled(GridListTileMaterialUI)`
+color: theme.palette.primary.light;
+`;
+
+export const GridListTileBar = styled(GridListTileBarMaterialUI)`
+background:
+        linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%);
+    
+`;
